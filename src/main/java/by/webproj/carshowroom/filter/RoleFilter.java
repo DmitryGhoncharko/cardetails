3f3fb0ca0a25ab7a2ebd7e6baf1f6a54ddd13d8b
dangeonMaster21 <dafae722@gmail.com>
2024-01-26 15:18:12 +0300

    complete

--- src/main/java/by/webproj/carshowroom/filter/RoleFilter.java
+++ src/main/java/by/webproj/carshowroom/filter/RoleFilter.java
@@ -9,17 +9,17 @@ public class RoleFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-//        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-//        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-//        String commandName = request.getParameter("command");
-//        if (httpServletRequest.getSession(false) == null || httpServletRequest.getSession(false).getAttribute("user") == null) {
-//            if (commandName == null || commandName.equals("login") || commandName.equals("logout") || commandName.equals("logincmnd") || commandName.equals("test") || commandName.equals("startTest") || commandName.equals("/")) {
-//                chain.doFilter(request,response);
-//            }else {
-//                httpServletResponse.sendRedirect("/controller?command=main_page");
-//            }
-//        }else {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        String commandName = request.getParameter("command");
+        if (httpServletRequest.getSession(false) == null ) {
+            if (commandName == null || commandName.equals("login") || commandName.equals("logout") || commandName.equals("logincmnd") || commandName.equals("test") || commandName.equals("startTest") || commandName.equals("/")) {
+                chain.doFilter(request,response);
+            }else {
+                httpServletResponse.sendRedirect("/controller?command=main_page");
+            }
+        }else {
             chain.doFilter(request, response);
-//        }
+        }
     }
 }
